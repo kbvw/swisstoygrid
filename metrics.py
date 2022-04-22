1e95e71314ad0442e7ce4eaaaa2b9c51fd2de9c6
kbvw <kbvw@pm.me>
2022-04-22 22:23:36 +0200

    Crude version of noise generation for simulations

--- metrics.py
+++ metrics.py
@@ -20,3 +20,46 @@ METRICS = {'max_loading_inner':   lambda net: net.res_line
 
 def create_metrics(metric_names):
     return [(metric_name, METRICS[metric_name]) for metric_name in metric_names]
+
+def apply_load_gen_noise(net, mean_outer_mw=10000, std_outer_mw=2000, mean_inner_mw=5000, std_inner_mw=1000):
+    import numpy as np
+    
+    # Total deviation in MW
+    dev_outer_mw = np.random.normal(mean_outer_mw, std_outer_mw)
+    dev_inner_mw = np.random.normal(mean_inner_mw, std_inner_mw)
+    
+    # Outer loads and generators
+    
+    # Generate noise
+    load_noise_outer = np.random.uniform(0, net.load.loc[5:8, 'p_mw'])
+    gen_noise_outer = np.random.uniform(0, net.gen.loc[5:8, 'p_mw'])
+    
+    # Normalize to distribution
+    load_noise_outer /= load_noise_outer.sum()
+    gen_noise_outer /= gen_noise_outer.sum()
+    
+    # Compute load and generator deviations in MW
+    load_noise_outer *= dev_outer_mw
+    gen_noise_outer *= dev_outer_mw
+    
+    # Apply noise to network
+    net.load.loc[5:8, 'p_mw'] += load_noise_outer
+    net.gen.loc[5:8, 'p_mw'] += gen_noise_outer
+    
+    # Inner loads and generators
+    
+    # Generate noise
+    load_noise_inner = np.random.uniform(0, net.load.loc[:4, 'p_mw'])
+    gen_noise_inner = np.random.uniform(0, net.gen.loc[:4, 'p_mw'])
+    
+    # Normalize to distribution
+    load_noise_inner /= load_noise_inner.sum()
+    gen_noise_inner /= gen_noise_inner.sum()
+    
+    # Compute load and generator deviations in MW
+    load_noise_inner *= dev_inner_mw
+    gen_noise_inner *= dev_inner_mw
+    
+    # Apply noise to network
+    net.load.loc[:4, 'p_mw'] += load_noise_inner
+    net.gen.loc[:4, 'p_mw'] += gen_noise_inner
